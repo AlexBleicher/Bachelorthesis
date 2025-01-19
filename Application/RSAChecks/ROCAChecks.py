528a1ef647826e50ae880a890eb263adde3ebb91
bleicheral88417 <bleicheral88417@th-nuernberg.de>
2025-01-19 19:16:12 +0100

    Added Copyright

--- Application/RSAChecks/ROCAChecks.py
+++ Application/RSAChecks/ROCAChecks.py
@@ -1,3 +1,5 @@
+#Usage of ROCA Detection Tool. Copyright (c) 2017, CRoCS, EnigmaBridge Ltd.
+
 from roca.detect import RocaFingerprinter
 from Application.Util.CreateWeaknessJSON import *
 import os
